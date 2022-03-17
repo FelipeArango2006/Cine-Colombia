7bf30fccc14f1005ecee90f3a3841fd7af5a73d3
FelipeArango2006 <feliparango2006@gmail.com>
2022-03-17 17:13:39 -0500

    asd

---
+++ reservas.js
@@ -0,0 +1,7 @@
+let contenedor = document.getElementById("contenedor")
+
+contenedor.addEventListener("click", function(evento){
+    if (evento.target.classList.contains("w-100")) {
+        evento.target.src="img/cyan.png"
+    }
+}) 
