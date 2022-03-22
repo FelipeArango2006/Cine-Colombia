5ff98fd369a1073710b170f59a0cfe31ddc0ed0d
FelipeArango2006 <feliparango2006@gmail.com>
2022-03-22 17:17:01 -0500

    nice

--- reservas.js
+++ reservas.js
@@ -1,7 +1,11 @@
-let contenedor = document.getElementById("contenedor")
+let contenedor = document.getElementById("contenedor");
 
 contenedor.addEventListener("click", function(evento){
     if (evento.target.classList.contains("w-100")) {
-        evento.target.src="img/cyan.png"
+        evento.target.src="stuff/cyan.png"
+        bandera = false
+    } else if(evento.target.classList.contains("w-100")) {
+        evento.target.src="stuff/cyan.png"
+        bandera = false
     }
 }) 
