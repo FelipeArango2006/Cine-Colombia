002f49bd7895b433c350b21c8063efa1863f8f5a
FelipeArango2006 <feliparango2006@gmail.com>
2022-03-23 16:52:53 -0500

    X

--- reservas.js
+++ reservas.js
@@ -9,3 +9,26 @@ contenedor.addEventListener("click", function(evento){
         bandera = false
     }
 }) 
+
+let envBtn = document.getElementById("boton");
+envBtn.addEventListener("click", escuchar);
+
+function escuchar(event) {
+    event.preventDefault();
+    let pelicula = document.getElementById("pelicula").value;
+    let cantidad = document.getElementById("cantidad").value;
+    let silla = document.getElementById("silla").value;
+    let boleta = 0
+    if(silla == "general") {
+        boleta = 10000
+    } else {
+        boleta= 15000
+    }
+
+    let total = boleta * cantidad;
+
+    console.log(pelicula);
+    console.log(cantidad);
+    console.log(silla);
+    alert("Precio total" + total + " pesos")
+}
